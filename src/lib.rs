103ca45ebc7d9bca8b134b9c55f24ad9b95e746e
Arbaaz <z.arbaaz786@gmail.com>
2025-11-29 01:39:03 +0000

    completed chapter 12 minigrep project

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut matching_lines: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            matching_lines.push(line);
+        }
+    }
+    matching_lines
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut matching_lines: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            matching_lines.push(line);
+        }
+    }
+    matching_lines
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_senstitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insenstitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
+use minigrep::{search, search_case_insensitive};
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    //dbg!(&args);
+
+    let input = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", input.query);
+
+    // read file
+    println!("In file {}", input.file_path);
+
+    if let Err(e) = run(input) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+    ignore_case: bool,
+}
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments passed.");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {query, file_path, ignore_case})
+    }    
+
+}
+
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else{
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+mod tests {
+    
+}
